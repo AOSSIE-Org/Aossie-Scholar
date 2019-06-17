78073a3a4ac13cd8d625b3f6b9b78ec656ef8a33
Manikaran <mani.singh_123@hotmail.com>
2019-06-17 12:21:27 +0530

    update

--- aossie_scholar/settings.py
+++ aossie_scholar/settings.py
@@ -16,7 +16,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'sleepy-oasis-20032.herokuapp.com']
 
