4478b946ea64361ba4134f71dfabd41641edc6d1
Manikaran <mani.singh_123@hotmail.com>
2019-06-18 14:54:51 +0530

    update

--- metrics/scrap.py
+++ metrics/scrap.py
@@ -52,7 +52,7 @@ class Scraper():
 
 			#response.close()	
 
-			page_soup = soup(response, "html.parser")		
+			page_soup = soup(page_html, "html.parser")		
 
 			if (j == 0):
 				Name= page_soup.find('div', {'id': 'gsc_prf_in'})			# extracting the author's name
