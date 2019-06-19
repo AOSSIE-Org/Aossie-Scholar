0e0e9efaaafebc458580cf1420f7e7c168604997
Manikaran <mani.singh_123@hotmail.com>
2019-06-19 22:50:54 +0530

    update

--- metrics/scrap.py
+++ metrics/scrap.py
@@ -9,13 +9,7 @@ from .extract import rawauthorscounterurl, seleniumScraper, coAuthors, getnewCit
 from metrics.newmetrics import Simple_Metrics
 import requests
 
-headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 'AppleWebKit/537.11 (KHTML, like Gecko) '
-                      'Chrome/23.0.1271.64 Safari/537.11',
-			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-			'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
-			'Accept-Encoding': 'none',
-			'Accept-Language': 'en-US,en;q=0.8',
-			'Connection': 'keep-alive'}
+
 
 class AppURLopener(urllib.request.FancyURLopener):
     version = "Mozilla/5.0"
@@ -49,6 +43,13 @@ class Scraper():
 			#response = opener.open(S_url)
 			#print (response)
 			#with urllib.request.urlopen(S_url, headers=headers) as response:
+			headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 'AppleWebKit/537.11 (KHTML, like Gecko) '
+                      'Chrome/23.0.1271.64 Safari/537.11',
+			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+			'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+			'Accept-Encoding': 'none',
+			'Accept-Language': 'en-US,en;q=0.8',
+			'Connection': 'keep-alive'}
 			req= Request(S_url, headers=headers)
 			response= urlopen(req).read()
 			#response= web_byte.decode('utf-8')
