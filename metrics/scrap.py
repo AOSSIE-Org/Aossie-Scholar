758b6dc63177ce850ba0d3837246d5046a80b6ab
Manikaran <mani.singh_123@hotmail.com>
2019-06-19 18:08:59 +0530

    update

--- metrics/scrap.py
+++ metrics/scrap.py
@@ -36,7 +36,8 @@ class Scraper():
 
 	
 		for j in range(0,pageSize, 100):		#{ looping trough pages to get all the publications
-			headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ',
+			headers= {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) ' 'AppleWebKit/537.11 (KHTML, like Gecko) '
+                      'Chrome/23.0.1271.64 Safari/537.11',
 			'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
 			'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
 			'Accept-Encoding': 'none',
