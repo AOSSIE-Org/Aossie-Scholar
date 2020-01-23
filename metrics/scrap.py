91e20b62be98dbaf78ebc16c716bd4dc35a3808c
Manikaran <mani.singh_123@hotmail.com>
2020-01-23 05:27:14 +0000

    cleaning scrap.py

--- metrics/scrap.py
+++ metrics/scrap.py
@@ -3,14 +3,9 @@ import urllib.request
 from bs4 import BeautifulSoup as soup 
 import re
 from django.utils import timezone
-from .extract import rawauthorscounterurl, seleniumScraper, coAuthors, getnewCitations, getNpapersNcitationsTcitations
+from metrics.extract import ScholarRawData 
 from .newmetrics import Simple_Metrics
 from .graphs import Graph
-
-title_list = []
-Citations =[]
-N_author_url= []
-author_names_list= []
 
 
 class Scraper():
@@ -20,60 +15,62 @@ class Scraper():
 		self.country= country
 
 	def getScholarData(self):
-		for i in range(0, 1000, 100):
-			if (self.maxP <=i):
-				pageSize = i
-				break        									# to count normalized papers.
+
 		title_list = []
 		Citations =[]
 		N_author_url= []
 		author_names_list= []
 		YEARS=[]
-		year_list=[]
-		ncounter = 0
-		bcounter= 0            												# to count total numbers citations an author recieved. 
+		for i in range(0, 1000, 100):
+			if (self.maxP <=i):
+				pageSize = i
+				break        									
 
-	#{ looping trough pages to get all the publications
-		for j in range(0,pageSize, 100):
+		# looping trough pages to get all the publications
 
+		for j in range(0,pageSize, 100): 
 			S_url=self.url + "&cstart=" + str(j) +"&pagesize=100"
-
 			with urllib.request.urlopen(S_url) as my_url:
-
 				page_html = my_url.read()			
+			page_soup = soup(page_html, "html.parser")	
 
-			page_soup = soup(page_html, "html.parser")		
+			# Informaiton to be scraped only from the first page (Scholar's Name, Picture, his/her website, Company)
 
-			if (j == 0):
-				Name= page_soup.find('div', {'id': 'gsc_prf_in'})			# extracting the author's name
-				scholar_name= Name.text
-				img = page_soup.find('img', {'id': 'gsc_prf_pup-img'})
-				img_url= img["src"]
-				Website = page_soup.findAll('a', {'class': 'gsc_prf_ila'})
-				work_info = page_soup.find('div', {'class': 'gsc_prf_il'})	
-				company= work_info.text	
-				counter=0
-				for site in Website:
-					if(len(Website)==2):
-						if(counter==0):
-							counter+=1
-							continue
-					website = site["href"]
+			if (j == 0): 				
+												
+				NameElement= page_soup.find('div', {'id': 'gsc_prf_in'})			
+				scholar_name= NameElement.text
 
-			Titles = page_soup.findAll('td', {'class': 'gsc_a_t'})			# publication titles
+				imageElement = page_soup.find('img', {'id': 'gsc_prf_pup-img'})
+				img_url= imageElement["src"]
 
-			for title in Titles:
+				WebsiteElement = page_soup.findAll('a', {'class': 'gsc_prf_ila'})
+				website=WebsiteElement[-1]["href"]
+			
+				work_infoElement = page_soup.find('div', {'class': 'gsc_prf_il'})	
+				company= work_infoElement.text	
+				
+				
+			#publication titles, title_list has all the titles of publications
+
+			TitleElement = page_soup.findAll('td', {'class': 'gsc_a_t'})	
+
+			for title in TitleElement:
 				Title = title.a.text
 				x=Title.encode('utf-8')
-				title_list.append(x.decode('utf-8', 'ignore'))				#title_list has all the titles
+				title_list.append(x.decode('utf-8', 'ignore'))	
+							
+			
+			# loop to get all the pop up urls embeded in publication
 
-			info_page = page_soup.findAll('a', {'class' : 'gsc_a_at'})
+			info_pageElement = page_soup.findAll('a', {'class' : 'gsc_a_at'})
 
-			for author in info_page:										# loop to get all the pop up urls and then collect number of co-authors from there
+			for auth in info_pageElement:										
 
-				Author_names_link = author["data-href"]
+				Author_names_link = auth["data-href"]
 
 				temp= re.findall("user=.+?[&]", Author_names_link)
+
 				user= temp[0][5:-1]
 
 				n_input=Author_names_link[-12:]
@@ -82,78 +79,65 @@ class Scraper():
 
 				N_author_url.append(n_author_url)
 
-			authors_soup= page_soup.findAll('div', {'class': 'gs_gray'})
+			# Scraping author names from the page only
 
-			less_authors_name=[]
+			authors_soupElement= page_soup.findAll('div', {'class': 'gs_gray'})
 
-			for a in authors_soup:
-				less_authors_name.append(a.text)
+			for i in range(0, len(authors_soupElement), 2):
+				author_names_list.append(authors_soupElement[i].text)
 
-			for i in range(0, len(less_authors_name), 2):
-				author_names_list.append(less_authors_name[i])
+			# Scraping and cleaning Citations data
 
-			Citations_soup = page_soup.findAll('td', {'class': 'gsc_a_c'})
+			Citations_soupElement = page_soup.findAll('td', {'class': 'gsc_a_c'})
 			
-			for c in Citations_soup:
+			for c in Citations_soupElement:
+
 				p= c.text.encode('utf-8')
-				r=p.decode('utf-8', 'ignore')
-				q= re.findall('[0-9]+',r)
-				Citations.append(q)
-		
-			Years = page_soup.findAll('td', {'class': 'gsc_a_y'})
-			for year in Years:
-				YEARS.append(year.text)
-
-			for year in Years:
+				r=p.decode('utf-8', 'ignore')	
 				try:
-					y= int(year.text)
-					year_list.append(y)
+					Citations.append(int(r))
 				except:
-					continue 
+					Citations.append(0)
+			
+			# Scraping published Year data and making it usable
+
+			YearsElement = page_soup.findAll('td', {'class': 'gsc_a_y'})
+
+			first_pub_year=float('Inf')
+			for year in YearsElement:
+				Yea=year.text
+				YEARS.append(Yea)
+				try:
+					if int(Yea)<first_pub_year:
+						first_pub_year=Yea
+				except:
+					continue
 				
+		#ScholarRawData class 
 
-		CoauthsAndUrls= rawauthorscounterurl(author_names_list)
+		scholarData=ScholarRawData()
+		scholarData.rawauthorscounterurl(author_names_list) 
+		scholarData.seleniumScraper(N_author_url)
+		number_of_coauths=scholarData.coAuthors()
+		scholarData.getNpapersNcitationsTcitations(Citations, len(title_list))
+		total_normalized_papers= scholarData.n_papers
+		normalized_citations= scholarData.n_citations
+		total_citations= scholarData.sum_citations
 
-		url_to_counter= CoauthsAndUrls[1]
-
-		n_author_names_list= CoauthsAndUrls[0]
-
-		coAuths= seleniumScraper(url_to_counter, N_author_url)
-
-		number_of_coauths= coAuthors(n_author_names_list, coAuths)
-
-		newCitations= getnewCitations(Citations)
-
-		myvar= getNpapersNcitationsTcitations(number_of_coauths, newCitations, len(title_list))
-
-		total_normalized_papers= myvar[0]
-
-		normalized_citations= myvar[1]
-
-		total_citations= myvar[2]
-
-		total_normalized_citations= int(sum(normalized_citations))
-		#normalized_h_index= int(sum(n_citations)/len(title_list))
-
-		nn_citations= normalized_citations[0:total_normalized_papers]
-		nn_citations.sort(reverse= True)
-
-		for i in nn_citations:
-			ncounter+= 1
-			if(ncounter> i):
-				normalized_h_index= ncounter-1
+		for i,j in enumerate(sorted(normalized_citations[:total_normalized_papers],reverse=True)):
+			if(i+1 > j):
+				normalized_h_index= i
 				break
 
 		Simple_Metrics_object= Simple_Metrics()
-		h_index= Simple_Metrics_object.h_index(newCitations)
-		g_index= Simple_Metrics_object.g_index(newCitations)
-		m_index= Simple_Metrics_object.m_index(h_index, min(year_list))
-		o_index= Simple_Metrics_object.o_index(h_index,max(newCitations))
-		h_median= Simple_Metrics_object.h_median(h_index,newCitations)
-		TNCc= Simple_Metrics_object.TNCc(total_normalized_citations, self.country)
-		print (TNCc)
+		h_index= Simple_Metrics_object.h_index(Citations)
+		g_index= Simple_Metrics_object.g_index(Citations)
+		m_index= Simple_Metrics_object.m_index(h_index, first_pub_year)
+		o_index= Simple_Metrics_object.o_index(h_index,max(Citations))
+		h_median= Simple_Metrics_object.h_median(h_index,Citations)
+		TNCc= Simple_Metrics_object.TNCc(sum(normalized_citations), self.country)
 
-		q= ScholarProfile(author_name= scholar_name, Company= company, Website= website, normalized_citations= normalized_citations, profile_url= user, publication_title= title_list, citations=newCitations,
+		q= ScholarProfile(author_name= scholar_name, Company= company, Website= website, normalized_citations= normalized_citations, profile_url= user, publication_title= title_list, citations=Citations,
 			coAuthors=number_of_coauths, country=self.country, Hmedian=h_median, Oindex= o_index, TNCc= TNCc, publications= len(title_list),Tcitations= total_citations, Year= YEARS, Gindex= g_index, Hindex= h_index, Mindex= m_index, created_at= timezone.now())
 		q.save()
 
