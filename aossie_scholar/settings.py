792c0fd3e3514837e5e02114925cf34d8d8bf0cd
adityabisoi <adityabisoi1999@gmail.com>
2020-02-11 10:26:31 +0530

    onfo page made responsive

--- aossie_scholar/settings.py
+++ aossie_scholar/settings.py
@@ -76,9 +76,9 @@ WSGI_APPLICATION = 'aossie_scholar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'DATABASE NAME HERE',         
-        'USER': 'USERNAME HERE',
-        'PASSWORD': 'PASSWORD HERE',
+        'NAME': 'aodb',         
+        'USER': 'postgres',
+        'PASSWORD': 'burger',
         'HOST': '127.0.0.1',                #Default host and ports
         'PORT': '5432',  
     }
