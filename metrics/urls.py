72a29a537c0c357368b09569cdad611da1188864
Sundaram Dubey <dubeysundaram057@gmail.com>
2020-02-23 12:02:29 +0000

    reverting

--- metrics/urls.py
+++ metrics/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 app_name= 'metrics'
 urlpatterns = [
-path('scholarly/', views.HomeView.as_view(), name='home'),
+path('', views.HomeView.as_view(), name='home'),
 path('<str:scholar_url>/results/', views.ResultView.as_view(), name= 'results'),
 path('info_page/', views.InfoPageView.as_view(), name= 'info_page'),
 path('search_results', views.SearchResultsView.as_view(), name='search_results')
