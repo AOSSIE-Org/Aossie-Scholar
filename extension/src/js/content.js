054de19f4f310ba918d393beff76541fab58e926
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-06-28 22:04:01 +0530

    raw data scraping added

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -4,4 +4,43 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
         const authorName = div.innerHTML
         sendResponse({content:authorName})                                              //Response sent only when on Scholar website
     }
+    else if (request.intent == 'scrape') {
+        var titleArray = []
+        var citArray = []
+        var yrArray = []
+        var titles = document.querySelectorAll('.gsc_a_at')
+        var citations = document.querySelectorAll('.gsc_a_ac')
+        var years = document.getElementsByClassName('gsc_a_h gsc_a_hc gs_ibl')
+        const img = document.getElementById('gsc_prf_pup-img').getAttribute('src')
+        for (var i = 0; i < titles.length; i++) {
+            var title = titles[i].textContent
+            var cit = citations[i].textContent
+            var year = years[i].textContent
+            titleArray.push(title)
+            citArray.push(cit)
+            yrArray.push(year)
+        }
+        sendResponse({
+            titles: titleArray,
+            citations:citArray,
+            years:yrArray,
+            image:img
+        })
+    }
+     else if (request.intent == 'loadBtn') {
+        function check() {
+            setTimeout(function () {
+                var btn = document.getElementById('gsc_bpf_more')
+                if (btn.getAttribute('disabled') == null) {
+                    btn.click()
+                    check()
+                }
+                 else{
+                    sendResponse({status:"true"})
+                }
+            }, 2500)
+        }
+        check()
+    }
+    return true
 })
