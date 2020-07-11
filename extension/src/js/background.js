a206e89211fc27fc289bf8576dc010a0b689d743
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-11 09:35:00 +0530

    background page added

---
+++ extension/src/js/background.js
@@ -0,0 +1,15 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.purpose = 'profileView') {
+
+        // Compute metrics
+
+        chrome.tabs.create({url: './profile.html'}, function () {
+            chrome.tabs.executeScript(null, {file: './profile.js'},function (e) {
+                    if (e == chrome.runtime.lastError) {
+                        // The extension isnt supposed to work on chrome://extensions URL
+                    }
+                    chrome.runtime.sendMessage(null, {scriptOptions: request})
+            })
+        })
+    }
+})
