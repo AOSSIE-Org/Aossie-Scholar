52b46badfaf328c7d381e99d5856699b5a585d69
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-11 09:36:37 +0530

    profile script

---
+++ extension/src/js/profile.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function profile() {
+    chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+
+        var data = request.scriptOptions
+
+        for(x of data.titles){
+            var d = document.getElementById('di')
+            var y = document.createElement('p')
+            y.innerHTML = x
+            d.appendChild(y)
+        }
+
+    })
+
+})
