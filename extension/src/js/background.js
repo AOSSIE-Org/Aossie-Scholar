e46b6b78bed55693fb150a02e7b8510d7791564e
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-11 09:39:03 +0530

    paths ficed

--- extension/src/js/background.js
+++ extension/src/js/background.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
 
         // Compute metrics
 
-        chrome.tabs.create({url: './profile.html'}, function () {
+        chrome.tabs.create({url: '../views/profile.html'}, function () {
             chrome.tabs.executeScript(null, {file: './profile.js'},function (e) {
                     if (e == chrome.runtime.lastError) {
                         // The extension isnt supposed to work on chrome://extensions URL
