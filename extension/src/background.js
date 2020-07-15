03b9af2f7534c010b400416a8a96fede35640de1
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-15 13:57:38 +0530

    demo POST from background script

--- extension/src/background.js
+++ extension/src/background.js
@@ -7,4 +7,12 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         if(request=='fromProfileJs'){
             sendResponse(arr)
         }
+        if (request.intent == 'sendToServer') {
+            axios.post('https://reqres.in/api/register',{
+                "email": "eve.holt@reqres.in",
+                "password": "pistol"    
+            })
+            .then((response)=>console.log(response))
+            .catch((error)=>console.log(error,error.response))
+        }
 })
