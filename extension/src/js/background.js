2c0d2c21862732851de74f601af839b835cc35f0
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-17 20:24:17 +0530

    data passing to localhost/api/

--- extension/src/js/background.js
+++ extension/src/js/background.js
@@ -8,10 +8,22 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             sendResponse(arr)
         }
         if (request.intent == 'sendToServer') {
-            axios.post('https://reqres.in/api/register',{
-                "email": "eve.holt@reqres.in",
-                "password": "pistol"    
-            })
+            axios.post('http://127.0.0.1:8000/api/',{
+            "author_name":request.scholarName,
+            "picture_url":request.scholarImage,
+            "workplace":request.workplace,
+            "publications":request.pubCount,
+            "total_citations":request.citCount,
+            "hindex":request.hIndex,
+            "gindex":request.gIndex,
+            "mindex":request.mIndex,
+            "oindex":request.oIndex,
+            "eindex":request.eIndex,
+            "hmedian":request.hMedian,
+            "publication_titles":request.titles,
+            "citations":request.citations,
+            "year":request.years
+        })
             .then((response)=>console.log(response))
             .catch((error)=>console.log(error,error.response))
         }
