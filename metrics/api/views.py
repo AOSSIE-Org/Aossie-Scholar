b4da85f91628c3e3975ee14c2501de4ee20a65e4
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-22 23:27:29 +0530

    api views changed

---
+++ metrics/api/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets, permissions
+from metrics.models import ScholarData
+from .serializers import ScholarSerializer
+from rest_framework.filters import SearchFilter
+
+class ScholarViewSet(viewsets.ModelViewSet):
+    queryset = ScholarData.objects.all()
+    permission_classes = [
+        permissions.AllowAny
+    ]
+    serializer_class = ScholarSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['scholarName']
