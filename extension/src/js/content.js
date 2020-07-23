2daf38002be0c1cee10c79fcdfef433dc7be8902
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-23 13:18:57 +0530

    co-author data extraction

--- extension/src/js/content.js
+++ extension/src/js/content.js
@@ -6,13 +6,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         scholarName = authorName
         sendResponse({
             content: authorName
-        }) //Response sent only when on Scholar website
+        })                                                                               //Response sent only when on Scholar website
     } else if (request.intent == 'scrape') {
         var titleArray = []
         var citArray = []
         var yrArray = []
+        var coAuthorArray = []
         var titles = document.querySelectorAll('.gsc_a_at')
         var citations = document.querySelectorAll('.gsc_a_ac')
+        var coAuthors = document.querySelectorAll('.gsc_a_tr .gsc_a_t')
         var years = document.getElementsByClassName('gsc_a_h gsc_a_hc gs_ibl')
         const img = document.getElementById('gsc_prf_pup-img').getAttribute('src')
         const workPlace = document.getElementsByClassName("gsc_prf_il")[0].innerText
@@ -27,6 +29,15 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             citArray.push(cit)
             yrArray.push(year)
         }
+        for(var i=0;i<titles.length;i++){
+            var authorsPerPublication = coAuthors[i].getElementsByClassName('gs_gray')[0].innerText
+            if(authorsPerPublication.indexOf('...')==-1){
+                coAuthorArray.push(authorsPerPublication.split(",").length)
+            }
+            else{
+                coAuthorArray.push(coAuthors[i].getElementsByClassName('gsc_a_at')[0].getAttribute('data-href'))
+            }
+        }
         chrome.runtime.sendMessage({
             intent: 'profileView',
             scholarName: scholarName,
@@ -36,7 +47,8 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
             image: img,
             workplace: workPlace,
             website: website,
-            country: country
+            country: country,
+            coAuthors: coAuthorArray
         })
     } else if (request.intent == 'loadBtn') {
         function check() {
@@ -50,7 +62,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
                         status: "true"
                     })
                 }
-            }, 2000)
+            }, 1000)
         }
         check()
     }
