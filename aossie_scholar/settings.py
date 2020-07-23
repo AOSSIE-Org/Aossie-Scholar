d537a69776a03ef7b0f6aa2f7d53d1f329fefe5b
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-07-23 12:41:07 +0530

    credentials removed

--- aossie_scholar/settings.py
+++ aossie_scholar/settings.py
@@ -75,9 +75,9 @@ WSGI_APPLICATION = 'aossie_scholar.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'rest',         
-        'USER': 'rest',
-        'PASSWORD': 'rest',
+        'NAME': 'DATABASE NAME HERE',         
+        'USER': 'USERNAME HERE',
+        'PASSWORD': 'PASSWORD HERE',
         'HOST': '127.0.0.1',                #Default host and ports
         'PORT': '5432',  
     }
