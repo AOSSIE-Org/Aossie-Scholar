8c5eefaf4ee86d77e830830d8bd2e11fbe20eed7
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-01 20:09:22 +0530

    configs updated

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    launch: {
+        headless: false,
+        slowMo: true,
+        devtools: true,
+    },
+}
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    preset: 'jest-puppeteer',
+    globals: {
+        URL: 'http://localhost:8080',
+    },
+    testMatch: ['**/test/**/*.test.js'],
+    verbose: true,
+}
