2d4724e92724b513739eff93da550b9f1c1e882a
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-02 12:49:29 +0530

    profile page e2e test added

--- test/profile.test.js
+++ test/profile.test.js
@@ -1,11 +1,12 @@
 const puppeteer = require('puppeteer')
 const extensionPath = 'src'
 
-test('new test', async () =>{
+test('Test profile page redirect', async () =>{
     const browser = await puppeteer.launch({
         headless: false, // extension are allowed only in the head-full mode
         slowMo: 80,
         args: [
+            '--window-size=1920,1080',
             `--disable-extensions-except=${extensionPath}`,
             `--load-extension=${extensionPath}`
         ]
@@ -14,7 +15,11 @@ test('new test', async () =>{
     const extensionPopupHtml = 'views/popup.html'
     const page = await browser.newPage();
     await page.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`);
-    const title = await page.title();
-    console.log(title)
-    // await browser.close();
-});
+    const newPagePromise = new Promise(resolve => browser.once('targetcreated', target => resolve(target.page())));
+    await page.click('button#searchBtn');
+    const newPage = await newPagePromise;
+    const testData = await newPage.$eval('.sidenav h3', el=>el.innerText);
+    await newPage.close();
+    await browser.close();
+    expect(testData).toBe('Publications');
+},10000);
