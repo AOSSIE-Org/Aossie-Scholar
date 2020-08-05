4b0adf0d341e00ded6161a0cce1ee9475f0c6861
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-05 22:02:53 +0530

    test

--- jest-puppeteer.config.js
+++ jest-puppeteer.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     launch: {
         headless: false,
-        slowMo: true,
+        slowMo: false,
         devtools: false,
     },
 }
--- test/profile.test.js
+++ test/profile.test.js
@@ -6,7 +6,7 @@ beforeAll(async () => {
     browser = await puppeteer.launch({
         headless: false, // extension are allowed only in the head-full mode
         slowMo: 80,
-        args: [`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`],
+        args: ['--no-sandbox',`--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`],
     })
     pages = await browser.pages()
     pages[0].close()
