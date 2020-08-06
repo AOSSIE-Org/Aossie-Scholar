5a7f5038c19feaad5ea921f4f04ff0beb9bf6800
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-06 11:37:35 +0530

    tests update

--- test/profile.test.js
+++ test/profile.test.js
@@ -2,24 +2,29 @@ const puppeteer = require('puppeteer')
 const extensionPath = 'src'
 let browser
 
-test('Test redirect to profile page', async () => {
+beforeAll(async () => {
     browser = await puppeteer.launch({
         headless: false, // extension are allowed only in the head-full mode
         slowMo: 100,
         args: ['--no-sandbox', `--disable-extensions-except=${extensionPath}`, `--load-extension=${extensionPath}`],
-        defaultViewport: {
-            width: 1300,
-            height: 600,
-            deviceScaleFactor: 1,
-        },
     })
+    pages = await browser.pages()
+    pages[0].close()
+})
+
+afterAll(() => {
+    browser.close()
+})
+
+test('Test redirect to profile page', async () => {
+    const extensionID = 'pfgmjkmlifhekiegffjndhpioapgcopk'
+    const extensionPopupHtml = 'views/popup.html'
     const page = await browser.newPage()
-    await page.goto('chrome-extension://pfgmjkmlifhekiegffjndhpioapgcopk/views/popup.html')
+    await page.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`)
     // const newPagePromise = new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page())))
     // await page.click('button#searchBtn')
     // const newPage = await newPagePromise
     // const testData = await newPage.$eval('.sidenav h3', (el) => el.innerText)
     // expect(testData).toBe('Publications')
-    await page.close()
-    await browser.close()
+    // await page.close()
 }, 10000)
