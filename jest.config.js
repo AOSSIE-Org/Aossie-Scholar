26fe56a3fbeae7a8bbdcf03ef6e78ccdd4eab6ea
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-06 23:50:33 +0530

    separate  unit and ee tests

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,13 @@
+let environment = process.env.environment
+let testMatch = []
+
+if (environment === 'unitonly') testMatch = ['**/test/metrics.test.js']
+if (environment === 'eeonly') testMatch = ['**/test/profile.test.js']
+
 module.exports = {
     preset: 'jest-puppeteer',
     testPathIgnorePatterns: ['/node_modules/'],
-    testMatch: ['**/test/*.test.js'],
+    testMatch,
     verbose: true,
     testEnvironment: 'jsdom',
 }
