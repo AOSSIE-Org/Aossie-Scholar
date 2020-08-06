2cc650988093e07db83f21ed928ebcb3601686b2
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-06 19:29:54 +0530

    test

--- test/profile.test.js
+++ test/profile.test.js
@@ -17,15 +17,14 @@ afterAll(() => {
 })
 
 test('Test redirect to profile page', async () => {
-    // const extensionID = 'pfgmjkmlifhekiegffjndhpioapgcopk'
-    // const extensionPopupHtml = 'views/popup.html'
-    // const page = await browser.newPage()
-    // await page.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`)
-    // const newPagePromise = new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page())))
-    // await page.click('button#searchBtn')
-    // const newPage = await newPagePromise
-    // const testData = await newPage.$eval('.sidenav h3', (el) => el.innerText)
-    // expect(testData).toBe('Publications')
-    // await page.close()
-    console.log(browser)
+    const extensionID = 'pfgmjkmlifhekiegffjndhpioapgcopk'
+    const extensionPopupHtml = 'views/popup.html'
+    const page = await browser.newPage()
+    await page.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`)
+    const newPagePromise = new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page())))
+    await page.click('button#searchBtn')
+    const newPage = await newPagePromise
+    const testData = await newPage.$eval('.sidenav h3', (el) => el.innerText)
+    expect(testData).toBe('Publications')
+    await page.close()
 }, 10000)
