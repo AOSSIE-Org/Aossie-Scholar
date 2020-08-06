846f7614cb7ac96da9a53ebba5b123c94e75228a
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-06 19:51:08 +0530

    test

--- test/profile.test.js
+++ test/profile.test.js
@@ -20,6 +20,7 @@ test('Test redirect to profile page', async () => {
     const extensionID = 'pfgmjkmlifhekiegffjndhpioapgcopk'
     const extensionPopupHtml = 'views/popup.html'
     const page = await browser.newPage()
+    console.log(browser)
     await page.goto(`chrome-extension://${extensionID}/${extensionPopupHtml}`)
     const newPagePromise = new Promise((resolve) => browser.once('targetcreated', (target) => resolve(target.page())))
     await page.click('button#searchBtn')
