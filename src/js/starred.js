8b13e97642b86eb73a046d0cda1f541072fec3b8
Aditya Bisoi <adityabisoi1999@gmail.com>
2020-08-08 23:56:56 +0530

    scholar fetch

--- src/js/starred.js
+++ src/js/starred.js
@@ -1,5 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
     chrome.runtime.sendMessage('fetchStarred', function (response) {
         console.log(response)
+        for (var i = 0; i < response.data.length; i += 1) {
+            const parentDiv = document.getElementById('parent-div')
+            const div = document.createElement('div')
+            div.setAttribute('id', 'name')
+            div.textContent = response.data[i].scholarName
+            parentDiv.appendChild(div)
+            const image = document.createElement('img')
+            image.setAttribute('id', 'picture')
+            image.setAttribute('src', response.data[i].scholarImage)
+            parentDiv.appendChild(image)
+            const p = document.createElement('p')
+            p.setAttribute('id', 'workplace')
+            p.textContent = response.data[i].workplace
+            const br = document.createElement('br')
+            parentDiv.appendChild(br)
+            parentDiv.appendChild(p)
+            const br1 = document.createElement('br')
+            parentDiv.appendChild(br1)
+            const hr = document.createElement('hr')
+            parentDiv.appendChild(hr)
+        }
     })
 })
