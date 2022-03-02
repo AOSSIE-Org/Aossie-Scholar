6a2b911f25081a06329da918cc8338707d47a34c
Subhangi Choudhary <subhangichoudhary6@gmail.com>
2022-03-02 22:58:05 +0530

    removed django_imports wherver unused

--- aossie_scholar/settings.py
+++ aossie_scholar/settings.py
@@ -1,6 +1,5 @@
 from dotenv import load_dotenv
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
