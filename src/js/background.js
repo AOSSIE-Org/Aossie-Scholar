94eb9ca99a957d322d8116989443c95f63739282
subhangichoudhary1997 <subhangichoudhary1997@gmail.com>
2022-08-21 01:57:02 +0530

    change axios to fetch API

--- src/js/background.js
+++ src/js/background.js
@@ -25,61 +25,27 @@ function createSearchResults() {
 }
 
 function checkDB(data) {
-    axios.get(`http://127.0.0.1:8000/api/?search=${data.scholarName}+${data.workplace}`).then((response) => {
-        const request = data
-        if (response.data[0] === undefined) {
-            axios
-                .post('http://127.0.0.1:8000/api/', {
-                    scholarName: request.scholarName,
-                    scholarImage: request.scholarImage,
-                    workplace: request.workplace,
-                    website: request.website,
-                    country: request.country,
-                    pubCount: request.pubCount,
-                    citCount: request.citCount,
-                    hIndex: request.hIndex,
-                    gIndex: request.gIndex,
-                    mIndex: request.mIndex,
-                    oIndex: request.oIndex,
-                    eIndex: request.eIndex,
-                    hMedian: request.hMedian,
-                    sIndex: request.sIndex,
-                    TNCc: request.TNCc,
-                    titles: request.titles,
-                    coauthors: request.coauthors,
-                    citations: request.citations,
-                    nCitations: request.nCitations,
-                    years: request.years,
-                })
-                .catch((error) => console.log(error))
-        } else {
-            const { id } = response.data[0]
-            axios
-                .put(`http://127.0.0.1:8000/api/${id}/`, {
-                    scholarName: request.scholarName,
-                    scholarImage: request.scholarImage,
-                    workplace: request.workplace,
-                    website: request.website,
-                    country: request.country,
-                    pubCount: request.pubCount,
-                    citCount: request.citCount,
-                    hIndex: request.hIndex,
-                    gIndex: request.gIndex,
-                    mIndex: request.mIndex,
-                    oIndex: request.oIndex,
-                    eIndex: request.eIndex,
-                    hMedian: request.hMedian,
-                    sIndex: request.sIndex,
-                    TNCc: request.TNCc,
-                    titles: request.titles,
-                    coauthors: request.coauthors,
-                    citations: request.citations,
-                    nCitations: request.nCitations,
-                    years: request.years,
-                })
-                .catch((error) => console.log(error))
-        }
-    })
+    fetch(`http://127.0.0.1:8000/api/?search=${data.scholarName}+${data.workplace}`)
+        .then((data) => {
+            return data.json()
+        })
+        .then((res) => {
+            const request = data
+            var response = {}
+            response.data = res
+            if (response.data[0] === undefined) {
+                fetch('http://127.0.0.1:8000/api/', {
+                    method: 'POST',
+                    body: JSON.stringify(request),
+                }).catch((error) => console.log(error))
+            } else {
+                const { id } = response.data[0]
+                fetch(`http://127.0.0.1:8000/api/${id}/`, {
+                    method: 'PUT',
+                    body: JSON.stringify(request),
+                }).catch((error) => console.log(error))
+            }
+        })
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
@@ -90,17 +56,23 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     }
     if (request.intent === 'search') {
         const searchTerm = request.searchTerm.split(' ').join('+')
-        axios.get(`http://127.0.0.1:8000/api?search=${searchTerm}`).then((response) => {
-            if (response.data.length === 1) {
-                arr = response
-                purpose = 'displayData'
-                createProfile()
-            } else {
-                arr = response
-                purpose = 'searchResults'
-                createSearchResults()
-            }
-        })
+        fetch(`http://127.0.0.1:8000/api?search=${searchTerm}`)
+            .then((data) => {
+                return data.json()
+            })
+            .then((res) => {
+                var response = {}
+                response.data = res
+                if (response.data.length === 1) {
+                    arr = response
+                    purpose = 'displayData'
+                    createProfile()
+                } else {
+                    arr = response
+                    purpose = 'searchResults'
+                    createSearchResults()
+                }
+            })
     }
     if (request.intent === 'createProfile') {
         arr = request.data
@@ -124,47 +96,75 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
         checkDB(request)
     }
     if (request.intent === 'checkStarred') {
-        axios.get(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`).then((response) => {
-            if (response.data[0] === undefined) {
-                sendResponse({ isStarred: 'false' })
-            } else {
-                var isStarred = response.data[0].isStarred
-                if (isStarred === true) {
-                    sendResponse({ isStarred: 'true' })
-                } else {
+        fetch(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`)
+            .then((data) => {
+                return data.json()
+            })
+            .then((res) => {
+                var response = {}
+                response.data = res
+                if (response.data[0] === undefined) {
                     sendResponse({ isStarred: 'false' })
+                } else {
+                    var isStarred = response.data[0].isStarred
+                    if (isStarred === true) {
+                        sendResponse({ isStarred: 'true' })
+                    } else {
+                        sendResponse({ isStarred: 'false' })
+                    }
                 }
-            }
-        })
+            })
     }
     if (request.intent === 'saveStarred') {
-        axios
-            .get(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`)
-            .then((response) => {
+        fetch(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`)
+            .then((data) => {
+                return data.json()
+            })
+            .then((res) => {
+                var response = {}
+                response.data = res
                 return response.data[0].id
             })
             .then((id) => {
-                axios.put(`http://127.0.0.1:8000/api/${id}/`, {
+                contents = {
                     isStarred: true,
+                }
+                fetch(`http://127.0.0.1:8000/api/${id}/`, {
+                    method: 'PUT',
+                    body: JSON.stringify(contents),
                 })
             })
     }
     if (request.intent === 'deleteStarred') {
-        axios
-            .get(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`)
-            .then((response) => {
+        fetch(`http://127.0.0.1:8000/api/?search=${request.name}+${request.work}`)
+            .then((data) => {
+                return data.json()
+            })
+            .then((res) => {
+                var response = {}
+                response.data = res
                 return response.data[0].id
             })
             .then((id) => {
-                axios.put(`http://127.0.0.1:8000/api/${id}/`, {
+                contents = {
                     isStarred: false,
+                }
+                fetch(`http://127.0.0.1:8000/api/${id}/`, {
+                    method: 'PUT',
+                    body: JSON.stringify(contents),
                 })
             })
     }
     if (request === 'fetchStarred') {
-        axios.get('http://127.0.0.1:8000/api/?search=true').then((data) => {
-            sendResponse(data)
-        })
+        fetch('http://127.0.0.1:8000/api/?search=true')
+            .then((data) => {
+                return data.json()
+            })
+            .then((res) => {
+                var response = {}
+                response.data = res
+                sendResponse(response)
+            })
     }
     return true
 })
