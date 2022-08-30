5ed160c5328fe4e2c1c5c54fe2d72315d37b02ce
subhangichoudhary1997 <subhangichoudhary1997@gmail.com>
2022-08-30 11:52:23 +0530

    e2e and unit testing using jest and pupeteer for indexes

--- test/metrics.test.js
+++ test/metrics.test.js
@@ -6,10 +6,13 @@ const {
     getMindex,
     getOindex,
     getHmedian,
+    getLindex,
+    getARindex,
 } = require('../src/js/profile')
 
 const citationArray = [69, 20, 19, 17, 10, 6, 5, 3, 2, 1, 1, 1]
 const years = [2020, 2021, 2022, 2023, 2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031]
+const coAuthors = [4, 8, 5, 8, 5, 11, 5, 2, 0, 1, 1]
 test('get h-index as 6', () => {
     expect(getHindex(citationArray)).toBe(6)
 })
@@ -40,4 +43,12 @@ test('get o-index as 20.35', () => {
 test('get h-median as 19', () => {
     const hIndex = getHindex(citationArray)
     expect(getHmedian(citationArray, hIndex)).toBe(19)
+})
+
+test('get l-index as 3.67', () => {
+    expect(getLindex(citationArray, coAuthors, years)).toBe(3.67)
+})
+
+test('get AR-index as 9.42', () => {
+    expect(getARindex(citationArray, years)).toBe(9.42)
 })
